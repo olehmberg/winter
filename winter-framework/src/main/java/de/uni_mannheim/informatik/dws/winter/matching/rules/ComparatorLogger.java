d9035bb00b5cc3ef184edf3229a3834379cd3354
abrinkmann <Alex0815@users.noreply.github.com>
2018-11-06 09:12:48 +0100

    Parallel Debug Log - Check Null Values

--- winter-framework/src/main/java/de/uni_mannheim/informatik/dws/winter/matching/rules/ComparatorLogger.java
+++ winter-framework/src/main/java/de/uni_mannheim/informatik/dws/winter/matching/rules/ComparatorLogger.java
@@ -8,16 +8,17 @@ import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Record;
 /**
  * @author Alexander Brinkmann (albrinkm@mail.uni-mannheim.de)
  *
- *	Logs the comparison logs per comparator and per thread if logging is activated.
+ *         Logs the comparison logs per comparator and per thread if logging is
+ *         activated.
  *
  */
 public class ComparatorLogger {
 
-	private ConcurrentHashMap<Long,Record> comparatatorLogPerThread = new ConcurrentHashMap<Long, Record>();
-	
-	public void initialise(){
-		 Thread currThread = Thread.currentThread();
-		 comparatatorLogPerThread.put(currThread.getId(), new Record(Long.toString(currThread.getId())));
+	private ConcurrentHashMap<Long, Record> comparatatorLogPerThread = new ConcurrentHashMap<Long, Record>();
+
+	public void initialise() {
+		Thread currThread = Thread.currentThread();
+		comparatatorLogPerThread.put(currThread.getId(), new Record(Long.toString(currThread.getId())));
 	}
 
 	public String getComparatorName() {
@@ -26,85 +27,120 @@ public class ComparatorLogger {
 	}
 
 	public void setComparatorName(String comparatorName) {
-		Record currRecord = comparatatorLogPerThread.get(Thread.currentThread().getId());
-		currRecord.setValue(COMPARATORNAME, comparatorName);
+		if (comparatatorLogPerThread.contains(Thread.currentThread().getId())) {
+			Record currRecord = comparatatorLogPerThread.get(Thread.currentThread().getId());
+			currRecord.setValue(COMPARATORNAME, comparatorName);
+		}
 	}
 
 	public String getRecord1Value() {
-		Record currRecord = comparatatorLogPerThread.get(Thread.currentThread().getId());
-		return currRecord.getValue(RECORD1VALUE);
+		if (comparatatorLogPerThread.contains(Thread.currentThread().getId())) {
+			Record currRecord = comparatatorLogPerThread.get(Thread.currentThread().getId());
+			return currRecord.getValue(RECORD1VALUE);
+		}
+		return null;
 	}
 
 	public void setRecord1Value(String record1Value) {
-		Record currRecord = comparatatorLogPerThread.get(Thread.currentThread().getId());
-		currRecord.setValue(RECORD1VALUE, record1Value);
+		if (comparatatorLogPerThread.contains(Thread.currentThread().getId())) {
+			Record currRecord = comparatatorLogPerThread.get(Thread.currentThread().getId());
+			currRecord.setValue(RECORD1VALUE, record1Value);
+		}
 	}
 
 	public String getRecord2Value() {
-		Record currRecord = comparatatorLogPerThread.get(Thread.currentThread().getId());
-		return currRecord.getValue(RECORD2VALUE);
+		if (comparatatorLogPerThread.contains(Thread.currentThread().getId())) {
+			Record currRecord = comparatatorLogPerThread.get(Thread.currentThread().getId());
+			return currRecord.getValue(RECORD2VALUE);
+		}
+		return null;
 	}
 
 	public void setRecord2Value(String record2Value) {
-		Record currRecord = comparatatorLogPerThread.get(Thread.currentThread().getId());
-		currRecord.setValue(RECORD2VALUE, record2Value);
+		if (comparatatorLogPerThread.contains(Thread.currentThread().getId())) {
+			Record currRecord = comparatatorLogPerThread.get(Thread.currentThread().getId());
+			currRecord.setValue(RECORD2VALUE, record2Value);
+		}
 	}
 
 	public String getRecord1PreprocessedValue() {
-		Record currRecord = comparatatorLogPerThread.get(Thread.currentThread().getId());
-		String Record1PreprocessedValue = currRecord.getValue(RECORD1PREPROCESSEDVALUE);
-		if (Record1PreprocessedValue == null) {
-			return getRecord1Value();
+		if (comparatatorLogPerThread.contains(Thread.currentThread().getId())) {
+			Record currRecord = comparatatorLogPerThread.get(Thread.currentThread().getId());
+			String Record1PreprocessedValue = currRecord.getValue(RECORD1PREPROCESSEDVALUE);
+			if (Record1PreprocessedValue == null) {
+				return getRecord1Value();
+			}
+			return Record1PreprocessedValue;
 		}
-		return Record1PreprocessedValue;
+		return null;
 	}
 
 	public void setRecord1PreprocessedValue(String record1PreprocessedValue) {
-		Record currRecord = comparatatorLogPerThread.get(Thread.currentThread().getId());
-		currRecord.setValue(RECORD1PREPROCESSEDVALUE, record1PreprocessedValue);
+		if (comparatatorLogPerThread.contains(Thread.currentThread().getId())) {
+			Record currRecord = comparatatorLogPerThread.get(Thread.currentThread().getId());
+			currRecord.setValue(RECORD1PREPROCESSEDVALUE, record1PreprocessedValue);
+		}
 	}
 
 	public String getRecord2PreprocessedValue() {
-		Record currRecord = comparatatorLogPerThread.get(Thread.currentThread().getId());
-		String Record2PreprocessedValue = currRecord.getValue(RECORD2PREPROCESSEDVALUE);
-		if (Record2PreprocessedValue == null) {
-			return getRecord2Value();
+		if (comparatatorLogPerThread.contains(Thread.currentThread().getId())) {
+			Record currRecord = comparatatorLogPerThread.get(Thread.currentThread().getId());
+			String Record2PreprocessedValue = currRecord.getValue(RECORD2PREPROCESSEDVALUE);
+			if (Record2PreprocessedValue == null) {
+				return getRecord2Value();
+			}
+			return Record2PreprocessedValue;
 		}
-		return Record2PreprocessedValue;
+		return null;
 	}
 
 	public void setRecord2PreprocessedValue(String record2PreprocessedValue) {
-		Record currRecord = comparatatorLogPerThread.get(Thread.currentThread().getId());
-		currRecord.setValue(RECORD2PREPROCESSEDVALUE, record2PreprocessedValue);
+		if (comparatatorLogPerThread.contains(Thread.currentThread().getId())) {
+			Record currRecord = comparatatorLogPerThread.get(Thread.currentThread().getId());
+			currRecord.setValue(RECORD2PREPROCESSEDVALUE, record2PreprocessedValue);
+		}
 	}
 
 	public String getSimilarity() {
-		Record currRecord = comparatatorLogPerThread.get(Thread.currentThread().getId());
-		return currRecord.getValue(SIMILARITY);
+		if (comparatatorLogPerThread.contains(Thread.currentThread().getId())) {
+			Record currRecord = comparatatorLogPerThread.get(Thread.currentThread().getId());
+			return currRecord.getValue(SIMILARITY);
+		}
+		return null;
 	}
 
 	public void setSimilarity(String similarity) {
-		Record currRecord = comparatatorLogPerThread.get(Thread.currentThread().getId());
-		currRecord.setValue(SIMILARITY, similarity);
+		if (comparatatorLogPerThread.contains(Thread.currentThread().getId())) {
+			Record currRecord = comparatatorLogPerThread.get(Thread.currentThread().getId());
+			currRecord.setValue(SIMILARITY, similarity);
+		}
 	}
 
 	public String getPostprocessedSimilarity() {
-		Record currRecord = comparatatorLogPerThread.get(Thread.currentThread().getId());
-		String postprocessedSimilarity = currRecord.getValue(POSTPROCESSEDSIMILARITY);
-		if (postprocessedSimilarity == null) {
-			return getSimilarity();
+		if (comparatatorLogPerThread.contains(Thread.currentThread().getId())) {
+			Record currRecord = comparatatorLogPerThread.get(Thread.currentThread().getId());
+			String postprocessedSimilarity = currRecord.getValue(POSTPROCESSEDSIMILARITY);
+			if (postprocessedSimilarity == null) {
+				return getSimilarity();
+			}
+			return postprocessedSimilarity;
 		}
-		return postprocessedSimilarity;
+		return null;
 	}
 
 	public void setPostprocessedSimilarity(String postprocessedSimilarity) {
-		Record currRecord = comparatatorLogPerThread.get(Thread.currentThread().getId());
-		currRecord.setValue(POSTPROCESSEDSIMILARITY, postprocessedSimilarity);
+		if (comparatatorLogPerThread.contains(Thread.currentThread().getId())) {
+			Record currRecord = comparatatorLogPerThread.get(Thread.currentThread().getId());
+			currRecord.setValue(POSTPROCESSEDSIMILARITY, postprocessedSimilarity);
+		}
 	}
-	
+
 	public String getValue(Attribute attribute) {
-		Record currRecord = comparatatorLogPerThread.get(Thread.currentThread().getId());
-		return currRecord.getValue(attribute);
+		if (comparatatorLogPerThread.contains(Thread.currentThread().getId())) {
+			Record currRecord = comparatatorLogPerThread.get(Thread.currentThread().getId());
+			return currRecord.getValue(attribute);
+		}
+		return null;
 	}
 
 	public static final Attribute COMPARATORNAME = new Attribute("comparatorName");
@@ -117,7 +153,6 @@ public class ComparatorLogger {
 
 	public static final Attribute[] COMPARATORLOG = { COMPARATORNAME, RECORD1VALUE, RECORD2VALUE,
 			RECORD1PREPROCESSEDVALUE, RECORD2PREPROCESSEDVALUE, SIMILARITY, POSTPROCESSEDSIMILARITY };
-
 
 	public boolean hasValue(Attribute attribute) {
 		if (attribute == COMPARATORNAME)
