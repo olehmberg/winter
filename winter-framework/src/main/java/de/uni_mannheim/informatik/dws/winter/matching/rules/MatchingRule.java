7147d0918a9291401a6ae16b5337ab06a801b7a7
abrinkmann <alex.brinkmann@informatik.uni-mannheim.de>
2021-01-21 17:47:04 +0100

    Identity Resolution Debug Log - DS Order
    Warn user if the order of DSs
    in GS and MatchingRule do not match

--- winter-framework/src/main/java/de/uni_mannheim/informatik/dws/winter/matching/rules/MatchingRule.java
+++ winter-framework/src/main/java/de/uni_mannheim/informatik/dws/winter/matching/rules/MatchingRule.java
@@ -422,11 +422,13 @@ public abstract class MatchingRule<RecordType extends Matchable, SchemaElementTy
 	 */
 	protected void addGoldstandardToDebugResults(){
 		if (this.comparatorLog != null && this.comparatorLogShort != null && debugGoldStandard != null) {
+			Boolean no_debug_record_found = true;
 			for(Pair<String, String> pair: this.debugGoldStandard.getPositiveExamples()){
 				String identifier = pair.getFirst()+ "-" + pair.getSecond();
 				Record debug = this.comparatorLog.getRecord(identifier);
 				if(debug != null){
 					debug.setValue(ATTRIBUTE_IS_MATCH, "1");
+					no_debug_record_found = false;
 				}
 			}
 			
@@ -435,8 +437,15 @@ public abstract class MatchingRule<RecordType extends Matchable, SchemaElementTy
 				Record debug = this.comparatorLog.getRecord(identifier);
 				if(debug != null){
 					debug.setValue(ATTRIBUTE_IS_MATCH, "0");
+					no_debug_record_found = false;
 				}
 			}
+
+			if(no_debug_record_found){
+				logger.warn("No corresponding record for the Debug Log found in the Goldstandard!");
+				logger.warn("Please align the order of Data Sets in Goldstandard and Matching Rule!");
+			}
+
 		}
 	}
 
